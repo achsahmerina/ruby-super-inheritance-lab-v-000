a508f9c101ef66c40d624901657ffc7d5a9fbb38
Achsah Abraham <achsahmp@gmail.com>
2018-10-01 18:05:48 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,7 @@ class ChattyStudent < Student
   def hello
     super
     puts "Hey there! I'm so ex
-cited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired.
+cited to learn stuff.How are you doing today? I'm okay, but I'm kind of tired.
  Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy
 ! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
